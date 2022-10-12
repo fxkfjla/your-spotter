909197efd09f887e55550dfd3c244ea2eb6f3b30
fxkfjla <kacper.skr11@gmail.com>
2022-10-12 21:15:25 +0200

    init

---
+++ src/main/java/com/example/YourspotterApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourspotterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourspotterApplication.class, args);
+	}
+
+}
