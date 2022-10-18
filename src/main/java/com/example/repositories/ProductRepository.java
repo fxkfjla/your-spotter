19f2ec8665f05de75e505b1628465feb5e1b3abc
fxkfjla <kacper.skr11@gmail.com>
2022-10-18 21:41:19 +0200

    add product repository

---
+++ src/main/java/com/example/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.example.repositories;
+
+import com.example.models.Product;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String>
+{
+
+}
