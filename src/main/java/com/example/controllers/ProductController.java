65bfdc8ccd7e0fe46f725949c2ffed502e61ecc0
fxkfjla <kacper.skr11@gmail.com>
2022-11-09 17:25:21 +0100

    add cross origin

--- src/main/java/com/example/controllers/ProductController.java
+++ src/main/java/com/example/controllers/ProductController.java
@@ -4,6 +4,7 @@ import com.example.models.Product;
 import com.example.services.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RequestMapping
 @RestController
 public class ProductController
