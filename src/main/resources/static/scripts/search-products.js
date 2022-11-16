eaa203dd0e97d91ba8be90c29f19c8b32c84bc77
fxkfjla <kacper.skr11@gmail.com>
2022-11-16 21:20:49 +0100

    new script for searching

---
+++ src/main/resources/static/scripts/search-products.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('form')
+const enterKey = 13;
+
+form.addEventListener('keypress', input =>
+{
+  if(input.keyCode === enterKey)
+  {
+    input.preventDefault()
+
+    localStorage.clear();
+    localStorage.setItem('counter', 0)
+
+    const value = input.target.value.toLowerCase()
+
+    fetch("/search=" + value)
+    .then(res => res.json())
+    .then(data =>
+    {
+      data.map(product =>
+      {
+        counter = localStorage.getItem('counter')
+
+        localStorage.setItem('name ' + counter, product.name)
+        localStorage.setItem('amount ' + counter, product.amount)
+        localStorage.setItem('price ' + counter, product.price)
+
+        counter++
+        localStorage.setItem('counter', counter)
+      })
+
+      location.href = "products.html"
+    })
+  }
+})
