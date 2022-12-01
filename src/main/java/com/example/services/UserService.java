d8c02b0ad665bcac6db1470912c02a92a4d23dc2
fxkfjla <kacper.skr11@gmail.com>
2022-12-01 01:45:55 +0100

    create user service

---
+++ src/main/java/com/example/services/UserService.java
@@ -0,0 +1,55 @@
+package com.example.services;
+
+import com.example.models.User;
+import com.example.repositories.UserRepository;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService
+{
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException
+    {
+        return userRepository.findByEmail(email).orElseThrow
+        (
+            ()-> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email))
+        );
+    }
+
+    public String signUpUser(User user)
+    {
+        // TODO: Fix nullpointer exception
+        // boolean userExists = userRepository.findByEmail(user.getEmail()).isPresent();
+
+        // if(userExists)
+        // {
+        //     // TODO: Handle exception
+        // }
+
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        // TODO: Autogenerate id
+
+        user.setId(2L);
+
+        userRepository.save(user);
+
+        // TODO: Send confirmation token
+
+        return "it works";
+    }
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final static String USER_NOT_FOUND_MSG = "user with email %s not found";
+}
