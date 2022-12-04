d81871cf0c37d810297232a1cb25ac4576dee627
fxkfjla <kacper.skr11@gmail.com>
2022-12-04 19:16:48 +0100

    add endpoint for email confirmation

--- src/main/java/com/example/controllers/RegistrationController.java
+++ src/main/java/com/example/controllers/RegistrationController.java
@@ -3,9 +3,11 @@ package com.example.controllers;
 import com.example.models.RegistrationRequest;
 import com.example.services.RegistrationService;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,12 @@ public class RegistrationController
     public String register(@RequestBody RegistrationRequest request)
     {
         return registrationService.register(request);
+    }
+
+    @GetMapping(path = "confirm")
+    public String confirm(@RequestParam("token") String token)
+    {
+        return registrationService.confirm(token);
     }
 
     private final RegistrationService registrationService;
