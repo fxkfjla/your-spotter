46c76d94205a1f938f6de4785b563ee60bd0eb26
fxkfjla <kacper.skr11@gmail.com>
2022-12-04 19:12:49 +0100

    add confirmation time function

--- src/main/java/com/example/services/ConfirmationTokenService.java
+++ src/main/java/com/example/services/ConfirmationTokenService.java
@@ -7,13 +7,34 @@ import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class ConfirmationTokenService
 {
-    public void saveConfirmationToken(ConfirmationToken token)
+    public void save(ConfirmationToken token)
     {
+        token.setId(repository.maxId() + 1L);
         repository.save(token);
+    }
+
+    ConfirmationToken getToken(String token)
+    {
+        Optional<ConfirmationToken> confirmationToken = repository.findByToken(token);
+
+        if(!confirmationToken.isPresent())
+        {
+            // TODO: Handle exception
+        }
+
+        return confirmationToken.get();
+    }
+
+    public void setConfirmedAt(ConfirmationToken token)
+    {
+        token.setConfirmedAt(LocalDateTime.now());
     }
 
     private final ConfirmationTokenRepository repository;
