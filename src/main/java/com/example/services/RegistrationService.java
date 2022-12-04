1986af48f881c7583998d169e658bd402efb6c9a
fxkfjla <kacper.skr11@gmail.com>
2022-12-04 19:11:34 +0100

    add email confirmation function

--- src/main/java/com/example/services/RegistrationService.java
+++ src/main/java/com/example/services/RegistrationService.java
@@ -1,13 +1,16 @@
 package com.example.services;
 
-import com.example.controllers.UserController;
+import com.example.models.ConfirmationToken;
 import com.example.models.RegistrationRequest;
 import com.example.models.UserRole;
 import com.example.models.User;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Service
@@ -22,12 +25,36 @@ public class RegistrationService
             // TODO: Handle exception
         }
 
-        return userController.signUpUser
+        return userService.signUpUser
         (
             new User(request.getEmail(), request.getPassword(), UserRole.USER)
         );
     }   
 
+    @Transactional
+    public String confirm(String token)
+    {
+        ConfirmationToken confirmationToken = tokenService.getToken(token);
+
+        if(confirmationToken.getConfirmedAt() != null)
+        {
+            return "email already confirmed";
+        }
+
+        LocalDateTime expiresAt = confirmationToken.getExpiresAt();
+        if(expiresAt.isBefore(LocalDateTime.now()))
+        {
+            // TODO: Handle exception
+            return "token expired";
+        }
+
+        tokenService.setConfirmedAt(confirmationToken);
+        userService.enableUser(confirmationToken.getUser().getEmail());
+
+        return "confirmed";
+    }
+
     private final EmailValidator emailValidator;
-    private final UserController userController;
+    private final UserService userService;
+    private final ConfirmationTokenService tokenService;
 }
