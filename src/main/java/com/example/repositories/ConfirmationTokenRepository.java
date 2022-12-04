75ee2f9d93313f1ab27ef6e08e70804d16fe9561
fxkfjla <kacper.skr11@gmail.com>
2022-12-04 19:15:32 +0100

    add highest id and find by token functions

--- src/main/java/com/example/repositories/ConfirmationTokenRepository.java
+++ src/main/java/com/example/repositories/ConfirmationTokenRepository.java
@@ -7,5 +7,6 @@ import java.util.Optional;
 public interface ConfirmationTokenRepository
 {
     public Optional<ConfirmationToken> findByToken(String token);
+    public Long maxId();
     public void save(ConfirmationToken token);
 }
--- src/main/java/com/example/repositories/ConfirmationTokenRepositoryImpl.java
+++ src/main/java/com/example/repositories/ConfirmationTokenRepositoryImpl.java
@@ -2,10 +2,13 @@ package com.example.repositories;
 
 import com.example.models.ConfirmationToken;
 
-import lombok.AllArgsConstructor;
-
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
+
+import lombok.AllArgsConstructor;
 
 import java.util.Optional;
 
@@ -16,7 +19,19 @@ public class ConfirmationTokenRepositoryImpl implements ConfirmationTokenReposit
     @Override
     public Optional<ConfirmationToken> findByToken(String token)
     {
-        return Optional.of(new ConfirmationToken());
+        Query query = new Query();
+        query.addCriteria(Criteria.where("token").regex(token));
+
+        return Optional.ofNullable(mongoTemplate.findOne(query, ConfirmationToken.class));
+    }
+
+    @Override
+    public Long maxId()
+    {
+        Query query = new Query().limit(1).with(Sort.by(Sort.Direction.DESC, "id"));
+        Optional<ConfirmationToken> token = Optional.ofNullable(mongoTemplate.findOne(query, ConfirmationToken.class));
+
+        return token.isPresent() ? token.get().getId() : -1L;
     }
 
     @Override
