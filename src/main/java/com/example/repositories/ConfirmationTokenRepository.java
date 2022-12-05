32206c9b16a3ba3355fee6e6894029c2fc146408
fxkfjla <kacper.skr11@gmail.com>
2022-12-05 23:01:50 +0100

    update token in repository function

--- src/main/java/com/example/repositories/ConfirmationTokenRepository.java
+++ src/main/java/com/example/repositories/ConfirmationTokenRepository.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 public interface ConfirmationTokenRepository
 {
     public Optional<ConfirmationToken> findByToken(String token);
+    public void confirmToken(ConfirmationToken token);
     public Long maxId();
     public void save(ConfirmationToken token);
 }
--- src/main/java/com/example/repositories/ConfirmationTokenRepositoryImpl.java
+++ src/main/java/com/example/repositories/ConfirmationTokenRepositoryImpl.java
@@ -6,10 +6,12 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
 import lombok.AllArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @AllArgsConstructor
@@ -23,6 +25,15 @@ public class ConfirmationTokenRepositoryImpl implements ConfirmationTokenReposit
         query.addCriteria(Criteria.where("token").regex(token));
 
         return Optional.ofNullable(mongoTemplate.findOne(query, ConfirmationToken.class));
+    }
+
+    @Override
+    public void confirmToken(ConfirmationToken token)
+    {
+        Query query = new Query().addCriteria(Criteria.where("token").regex(token.getToken()));
+        Update update = new Update().set("confirmedAt", LocalDateTime.now());
+
+        mongoTemplate.updateFirst(query, update, ConfirmationToken.class);
     }
 
     @Override
