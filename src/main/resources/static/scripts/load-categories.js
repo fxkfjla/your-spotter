8525139ae042251e899d6b428d496b551364acc9
fxkfjla <kacper.skr11@gmail.com>
2022-12-07 12:02:45 +0100

    load product categories

---
+++ src/main/resources/static/scripts/load-categories.js
@@ -0,0 +1,19 @@
+const categoryTemplate = document.querySelector("[category-template]");
+const categoriesContainer = document.querySelector("[categories-container]")
+
+fetch("products/categories/all")
+.then(res => res.json())
+.then(data =>
+{
+    data.forEach(category =>
+    {
+        const template = categoryTemplate.content.cloneNode(true).children[0];
+        const image = template.querySelector("[category-image]")
+        const name = template.querySelector("[category-name]")
+
+        name.textContent = category.name
+        // image.textContent = category.image
+
+        categoriesContainer.append(template)
+    }); 
+})
