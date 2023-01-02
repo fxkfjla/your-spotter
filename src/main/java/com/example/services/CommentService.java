9d36f60564329f8ece74440f01c4426ed4bf3135
Hubert Jonak <jonakhubert@gmail.com>
2023-01-02 18:48:30 +0100

    Added comment service

---
+++ src/main/java/com/example/services/CommentService.java
@@ -0,0 +1,30 @@
+package com.example.services;
+
+import com.example.models.Comment;
+import com.example.repositories.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentService
+{
+    @Autowired
+    public CommentService(CommentRepository commentRepository)
+    {
+        this.commentRepository = commentRepository;
+    }
+
+    public List<Comment> getByProductId(int id)
+    {
+        return commentRepository.findByProductId(id);
+    }
+
+    public void addComment(Comment comment)
+    {
+        commentRepository.addComment(comment);
+    }
+
+    private final CommentRepository commentRepository;
+}
