a47c2155e2932698e7eb83548cb365192491b438
fxkfjla <kacper.skr11@gmail.com>
2023-01-02 22:13:30 +0100

    fix comment posting

--- src/main/resources/static/scripts/load-product.js
+++ src/main/resources/static/scripts/load-product.js
@@ -1,52 +1,58 @@
-const form = document.getElementById('form');
-
 var product = document.querySelector("[product]")
 
 var value = localStorage.getItem('product')
 
 if(value != null)
 {
-    fetch("/products/search?name=" + value)
-    .then(res => res.json())
-    .then(data =>
+  fetch("/products/search?name=" + value)
+  .then(res => res.json())
+  .then(data =>
+  {
+    data.forEach(result =>
     {
-      data.forEach(result =>
+      var image = product.querySelector("[product-image]")
+      var name = product.querySelector("[product-name]")
+      var price = product.querySelector("[product-price]")
+
+      image.src = result.imageUrl
+      name.textContent = result.name
+      price.textContent = result.price + " PLN"
+
+      var postButton = document.querySelector(".post-button")
+      
+      postButton.addEventListener('click', input => 
       {
-        var image = product.querySelector("[product-image]")
-        var name = product.querySelector("[product-name]")
-        var price = product.querySelector("[product-price]")
-
-        image.src = result.imageUrl
-        name.textContent = result.name
-        price.textContent = result.price + " PLN"
+        input.preventDefault();
+      
+        comment = document.getElementById('comment').value;
         
-        form.addEventListener('click', input => 
+        fetch("comments/addComment", 
         {
-          input.preventDefault();
-        
-          const comment = document.getElementById('comment').value;
-          
-          fetch("comments/addComment", {
-            method: 'POST',
-            headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ "comment": comment, "product": result })
-          });
-        });
-
-        fetch("/comments/productId?id=" + result.id)
-        .then(res => res.json())
-        .then(comments =>
-        {
-          section = document.querySelector(".comments")
-        
-          comments.forEach(comment =>
+          method: 'POST',
+          headers: 
           {
-            section.innerHTML += "<div class='product-opinion'><div class='text'>" + comment.comment + "</div></div>";
-          }) 
-        })
-      }); 
-    })
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ "comment": comment, "product": result })
+        }).then(() => location.href = "/product.html");
+      });
+
+      fetch("/comments/productId?id=" + result.id)
+      .then(res => res.json())
+      .then(comments =>
+      {
+        section = document.querySelector(".comments")
+      
+        comments.forEach(comment =>
+        {
+          var commentDiv = document.createElement('div');
+          commentDiv.classList.add('product-opinion');
+          commentDiv.innerHTML = "<div class='text'>" + comment.comment + "</div>";
+
+          section.appendChild(commentDiv);
+        }) 
+      })
+    }); 
+  })
 }
