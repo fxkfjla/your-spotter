33ecacc8cb0358f4a07742d336cd0f12a87e16fd
Hubert Jonak <jonakhubert@gmail.com>
2023-01-02 21:31:22 +0100

    Added html implementation for adding comments

--- src/main/resources/static/scripts/load-product.js
+++ src/main/resources/static/scripts/load-product.js
@@ -1,3 +1,5 @@
+const form = document.getElementById('form');
+
 var product = document.querySelector("[product]")
 
 var value = localStorage.getItem('product')
@@ -8,15 +10,43 @@ if(value != null)
     .then(res => res.json())
     .then(data =>
     {
-        data.forEach(result =>
-        {
-            var image = product.querySelector("[product-image]")
-            var name = product.querySelector("[product-name]")
-            var price = product.querySelector("[product-price]")
+      data.forEach(result =>
+      {
+        var image = product.querySelector("[product-image]")
+        var name = product.querySelector("[product-name]")
+        var price = product.querySelector("[product-price]")
 
-            image.src = result.imageUrl
-            name.textContent = result.name
-            price.textContent = result.price + " PLN"
-        }); 
+        image.src = result.imageUrl
+        name.textContent = result.name
+        price.textContent = result.price + " PLN"
+        
+        form.addEventListener('click', input => 
+        {
+          input.preventDefault();
+        
+          const comment = document.getElementById('comment').value;
+          
+          fetch("comments/addComment", {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ "comment": comment, "product": result })
+          });
+        });
+
+        fetch("/comments/productId?id=" + result.id)
+        .then(res => res.json())
+        .then(comments =>
+        {
+          section = document.querySelector(".comments")
+        
+          comments.forEach(comment =>
+          {
+            section.innerHTML += "<div class='product-opinion'><div class='text'>" + comment.comment + "</div></div>";
+          }) 
+        })
+      }); 
     })
 }
