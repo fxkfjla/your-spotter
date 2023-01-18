567809bddcc88005f542813cfac18113d6f2878b
Hubert Jonak <jonakhubert@gmail.com>
2023-01-18 14:42:41 +0100

    Added data field to Comment model

--- src/main/java/com/example/models/Comment.java
+++ src/main/java/com/example/models/Comment.java
@@ -13,11 +13,12 @@ import org.springframework.data.mongodb.core.mapping.Field;
 @NoArgsConstructor
 public class Comment
 {
-    public Comment(String comment, Product product, String userEmail)
+    public Comment(String comment, Product product, String userEmail, String data)
     {
         this.comment = comment;
         this.product = product;
         this.userEmail = userEmail;
+        this.data = data;
     }
 
     @Field
@@ -29,4 +30,7 @@ public class Comment
 
     @Field
     private String userEmail;
+
+    @Field
+    private String data;
 }
